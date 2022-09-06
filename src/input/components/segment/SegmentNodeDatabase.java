89c6af63ae943c5ac9832ccd20c24e6e421f516a
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-06 19:36:12 -0400

    SegmentNodeDatabase Constructors

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -1,19 +1,24 @@
 package input.components.segment;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import input.components.point.PointNode;
 
-public class SegmentNodeDatabase {
+public class SegmentNodeDatabase 
+{
+	protected Map<PointNode, Set<PointNode>> _adjLists;
+	
 	public SegmentNodeDatabase()
 	{
-		
+		this._adjLists = new HashMap<PointNode, Set<PointNode>>();
 	}
 	public SegmentNodeDatabase(Map<PointNode, Set<PointNode>> _set) 
 	{
-		
+		this._adjLists = new HashMap<PointNode, Set<PointNode>>();
+		this._adjLists.putAll(_set);
 	}
 	public int numUndirectedEdges() 
 	{
@@ -25,7 +30,7 @@ public class SegmentNodeDatabase {
 	public void addUndirectedEdge(PointNode a, PointNode b) {
 		
 	}
-	public void addAdjacencyList(PointNode a, PointNode b) {
+	public void addAdjacencyList(PointNode a, List<PointNode> b) {
 		
 	}
 	public List<SegmentNode> asSegmentList(){
